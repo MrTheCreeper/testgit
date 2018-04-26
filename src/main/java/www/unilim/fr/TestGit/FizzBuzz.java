22f57e63d324b1118e7a7a084787a5e562314b26
iut <iut@10.0.2.15>
2018-04-26 11:42:29 +0200

    commit fizzbuzz

--- src/main/java/www/unilim/fr/TestGit/FizzBuzz.java
+++ src/main/java/www/unilim/fr/TestGit/FizzBuzz.java
@@ -2,4 +2,7 @@ package www.unilim.fr.TestGit;
 
 public class FizzBuzz {
 
-}
+    public static String getResult() {
+	    return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz";
+    }
+}  
--- src/main/java/www/unilim/fr/TestGit/HelloWorld.java
+++ src/main/java/www/unilim/fr/TestGit/HelloWorld.java
@@ -2,9 +2,13 @@ package www.unilim.fr.TestGit;
 
 public class HelloWorld {
 	
-	public static void main( String[] args){
-		System.out.println("Hello World");
-		System.out.println("Jouons ensemble!");
-	}
+    public static void main( String[] args )
+        {
+			System.out.println( "Hello World!" );
+		System.out.println( "Jouons ensemble!" );  
+		
+		System.out.println( "Et si on jouait au FizzBuzz ?" );  
+        System.out.println(FizzBuzz.getResult());
+    }
+}  
 
-}
